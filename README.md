8f6eaad7ebfc222e62e5b5b0d9dffbea17a80193
Alexander L <ropptar@ya.ru>
2025-06-24 14:56:47 +0300

    basic layout

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ Here are some patches to dwmblocks that add features that I either don't want to
 I do not maintain these but I will take pull requests to update them.
 <br>
 <a href=https://gist.github.com/IGeraGera/e4a5583b91b3eec2e81fdceb44dea717>dwmblocks-statuscmd-b6b0be4.diff</a>
+
+scripts are in my [dotfiles repo](https://github.com/ropptar/dotfiles/tree/master/scripts/bar)
---
+++ blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Usage",	"",				5,					0},
+	{"Music",	"",				5,					0},
+	{"",		"bar-keymap",	0,					1},
+	{"",		"bar-clock",	5,					0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
