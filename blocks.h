4b9ebe6955bce37d588079f961a24cc97901c534
Alexander L <ropptar@ya.ru>
2025-06-29 23:22:50 +0300

    usage, todo music(MPD)

--- blocks.h
+++ blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Usage",	"",				5,					0},
+	{"",		"bar-usage",	15,					0},
 	{"Music",	"",				5,					0},
 	{"",		"bar-keymap",	0,					1},
-	{"",		"bar-clock",	5,					0},
+	{"",		"bar-clock",	60,					0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
